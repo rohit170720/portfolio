b36ebd777148c096e69ef630ec5829a6cc645a20
Rohit Renke <prrro1707@gmail.com>
2025-04-04 20:04:05 +0530

    Changed base Path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
+  basePath: "/portfolio",
 };
 
 export default nextConfig;
